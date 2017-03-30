632c8089156106bc719493052a72848d1b2f28c1
dwalsh44 <G00333279@gmit.ie>
2017-03-30 09:43:44 +0100

    Quiz project
    Quiz version 1 multi page quiz

---
+++ js/quiz.js
@@ -0,0 +1,11 @@
+var questions = [
+['How many moons does Earth have?',1],
+['What is the capital of Italy?','rome'],
+['How many moons does Saturn have?',31],
+['How many moons does Mars have?',2],
+['What is the capital of Estonia?', 'tallinn'],
+['What is the capital of Spain?','madrid'],
+['How many moons does Venus have?',0],
+['What is the capital of Australia?','canberra'],
+
+];
